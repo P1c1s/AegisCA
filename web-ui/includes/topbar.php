787e3d42a41dfaa11fc33470a9122b5ac46125dc
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2026-07-05 18:43:03 +0200

    Responsive
    - make topbar responsive and update style.css

--- web-ui/includes/topbar.php
+++ web-ui/includes/topbar.php
@@ -4,11 +4,14 @@
         Aegis Certificate Authorities
     </div>
     <div class="nav-links">
-        <a href="index.php">Dashboard</a>
-        <a href="manage_ca.php">Gestione CA</a>
-        <a href="manage_certs.php">Gestione Certificati</a>
-        <a href="import.php">Importa</a> 
-        <a href="profile.php">Profilo</a>
-        <a href="logout.php" class="logout">Logout</a>
+        <a href="index.php"><i class="fa-solid fa-chart-pie"></i> <span>Dashboard</span></a>
+        <a href="manage_ca.php"><i class="fa-solid fa-shield-halved"></i> <span>Gestione CA</span></a>
+        
+        <!-- Esempio se usi l'icona singola o lo stack di prima, l'importante è isolare la scritta -->
+        <a href="manage_certs.php"><i class="fa-solid fa-award"></i> <span>Gestione Certificati</span></a>
+        
+        <a href="import.php"><i class="fa-solid fa-file-import"></i> <span>Importa</span></a> 
+        <a href="profile.php"><i class="fa-solid fa-user"></i> <span>Profilo</span></a>
+        <a href="logout.php" class="logout"><i class="fa-solid fa-right-from-bracket"></i> <span>Logout</span></a>
     </div>
 </div>
