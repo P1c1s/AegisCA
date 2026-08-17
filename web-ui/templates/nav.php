fac9a4a0106bb2da970eae38c3f471185b7e666b
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2026-08-17 09:54:05 +0200

    fix(nav): correct translation keys prefix from topbar to nav in nav.php
    - Update `topbar_*` to `nav_*` in `src/Components/nav.php`
    - Align translation key calls with the renamed navigation component

--- web-ui/templates/nav.php
+++ web-ui/templates/nav.php
@@ -6,13 +6,13 @@
         Aegis Certificate Authorities
     </div>
     <div class="nav-links">
-        <a href="index.php?page=dashboard"><i class="fa-solid fa-chart-pie"></i> <span><?= __('topbar_dashboard', 'Dashboard') ?></span></a>
-        <a href="index.php?page=manage_ca"><i class="fa-solid fa-shield-halved"></i> <span><?= __('topbar_manage_ca', 'CA Management') ?></span></a>
-        <a href="index.php?page=manage_certs"><i class="fa-solid fa-award"></i> <span><?= __('topbar_manage_certs', 'Certificates Management') ?></span></a>
+        <a href="index.php?page=dashboard"><i class="fa-solid fa-chart-pie"></i> <span><?= __('nav_dashboard', 'Dashboard') ?></span></a>
+        <a href="index.php?page=manage_ca"><i class="fa-solid fa-shield-halved"></i> <span><?= __('nav_manage_ca', 'CA Management') ?></span></a>
+        <a href="index.php?page=manage_certs"><i class="fa-solid fa-award"></i> <span><?= __('nav_manage_certs', 'Certificates Management') ?></span></a>
         
-        <a href="index.php?page=import"><i class="fa-solid fa-file-import"></i> <span><?= __('topbar_import', 'Import') ?></span></a> 
-        <a href="index.php?page=profile"><i class="fa-solid fa-user"></i> <span><?= __('topbar_profile', 'Profile') ?></span></a>
+        <a href="index.php?page=import"><i class="fa-solid fa-file-import"></i> <span><?= __('nav_import', 'Import') ?></span></a> 
+        <a href="index.php?page=profile"><i class="fa-solid fa-user"></i> <span><?= __('nav_profile', 'Profile') ?></span></a>
 
-        <a href="index.php?page=logout" class="logout"><i class="fa-solid fa-right-from-bracket"></i> <span><?= __('topbar_logout', 'Logout') ?></span></a>
+        <a href="index.php?page=logout" class="logout"><i class="fa-solid fa-right-from-bracket"></i> <span><?= __('nav_logout', 'Logout') ?></span></a>
     </div>
 </div>
