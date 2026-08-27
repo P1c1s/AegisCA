7aa23a8e6d9022bc64b4210ebc317f6977384670
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2026-08-27 20:27:08 +0200

    refactor(dev): update Dockerfile.dev paths and rename build script
    - Fixed build context COPY paths in 'dev/docker/Dockerfile.dev'.
    - Replaced 'local-builder.sh' with 'dev-builder.sh' and updated build arguments and paths.

--- dev/script/dev-builder.sh
+++ dev/script/dev-builder.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-# Silenzia gli errori se il container non esiste o è già fermo
-docker stop aegis-ca-local 2>/dev/null || true
-docker rm aegis-ca-local 2>/dev/null || true
+# Determina il percorso della root del progetto dinamicamente
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 # Legge la versione dal file VERSION
 APP_VERSION=$(< ../VERSION)
+
+# Silenzia gli errori se il container non esiste o è già fermo
+docker stop aegis-ca-dev 2>/dev/null || true
+docker rm aegis-ca-dev 2>/dev/null || true
 
 # DOCKERFILE="../docker/Dockerfile"
 DOCKERFILE="../docker/Dockerfile.dev"
@@ -16,20 +19,19 @@ DOCKERFILE="../docker/Dockerfile.dev"
 docker build \
   -f $DOCKERFILE \
   --build-arg APP_VERSION="$APP_VERSION" \
-  -t aegis-ca-local \
-  --no-cache ..
+  -t aegis-ca-dev \
+  --no-cache \
+  "$PROJECT_ROOT"
 
-# Determina il percorso della root del progetto dinamicamente
-PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # Avvia il nuovo container per fare i test
 docker run -it \
-  --name aegis-ca-local \
-  --hostname aegis-ca-local \
+  --name aegis-ca-dev \
+  --hostname aegis-ca-dev \
   -e TZ=Europe/Rome \
   -p 8080:80 \
+  -v /tmp/aegis-ca-dev_data:/data \
   -v "$PROJECT_ROOT/web-ui":/var/www/localhost/htdocs/ \
+  -v "$PROJECT_ROOT/dev/php":/php \
   -v "$PROJECT_ROOT/python":/python \
-  aegis-ca-local
-
-  #-v /tmp/aegis-ca-local_data:/data \
+  aegis-ca-dev
